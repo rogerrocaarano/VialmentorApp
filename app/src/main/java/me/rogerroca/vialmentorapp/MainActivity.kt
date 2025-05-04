a4c945ccb7ac4202a1496755db29e2e015c6581c
Roger Roca Arano <git@rogerroca.me>
2025-05-04 02:08:14 -0400

    refactor: move navigation logic to MyApp composable

--- app/src/main/java/me/rogerroca/vialmentorapp/MainActivity.kt
+++ app/src/main/java/me/rogerroca/vialmentorapp/MainActivity.kt
@@ -5,25 +5,11 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
-import me.rogerroca.vialmentorapp.data.local.room.AppDb
-import me.rogerroca.vialmentorapp.data.local.room.LocalConversationRepository
-import me.rogerroca.vialmentorapp.data.local.room.LocalMessageRepository
-import me.rogerroca.vialmentorapp.ui.screen.ConversationScreen
-import me.rogerroca.vialmentorapp.ui.screen.ConversationsListScreen
-import me.rogerroca.vialmentorapp.ui.theme.VialmentorAppTheme
-import me.rogerroca.vialmentorapp.viewmodel.ConversationViewModel
-import me.rogerroca.vialmentorapp.viewmodel.ConversationsListViewModel
 
 class MainActivity : ComponentActivity() {
     private lateinit var auth: FirebaseAuth
@@ -42,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             var showDialog by remember { mutableStateOf(false) }
-            MyScreenContent(this)
+            MyApp(this)
         }
     }
 
@@ -58,46 +44,5 @@ class MainActivity : ComponentActivity() {
                     Log.e("MainActivity", "Authentication failed", task.exception)
                 }
             }
-    }
-
-    @Composable
-    fun MyScreenContent(activity: MainActivity) {
-        val database = AppDb.getDatabase(activity)
-        val messageDao = database.messageDao()
-        val conversationDao = database.conversationDao()
-        val localMessageRepository = LocalMessageRepository(messageDao)
-        val localConversationRepository = LocalConversationRepository(conversationDao)
-        val navController = rememberNavController()
-
-        VialmentorAppTheme {
-            NavHost(
-                navController = navController,
-                startDestination = "conversationsList"
-            ) {
-
-                composable("conversationsList") {
-                    ConversationsListScreen(
-                        ConversationsListViewModel(localConversationRepository),
-                        navController = navController,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-
-                composable("conversation/{conversationId}") { backStackEntry ->
-                    val conversationId =
-                        backStackEntry.arguments?.getString("conversationId")?.toInt()
-                    require(conversationId is Int)
-                    ConversationScreen(
-                        ConversationViewModel(
-                            localMessageRepository,
-                            localConversationRepository,
-                            conversationId
-                        ),
-                        conversationId = conversationId,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-            }
-        }
     }
 }
---
+++ app/src/main/java/me/rogerroca/vialmentorapp/MyApp.kt
@@ -0,0 +1,57 @@
+package me.rogerroca.vialmentorapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import me.rogerroca.vialmentorapp.data.local.room.AppDb
+import me.rogerroca.vialmentorapp.data.local.room.LocalConversationRepository
+import me.rogerroca.vialmentorapp.data.local.room.LocalMessageRepository
+import me.rogerroca.vialmentorapp.ui.screen.ConversationScreen
+import me.rogerroca.vialmentorapp.ui.screen.ConversationsListScreen
+import me.rogerroca.vialmentorapp.ui.theme.VialmentorAppTheme
+import me.rogerroca.vialmentorapp.viewmodel.ConversationViewModel
+import me.rogerroca.vialmentorapp.viewmodel.ConversationsListViewModel
+
+@Composable
+fun MyApp(activity: MainActivity) {
+    val database = AppDb.getDatabase(activity)
+    val messageDao = database.messageDao()
+    val conversationDao = database.conversationDao()
+    val localMessageRepository = LocalMessageRepository(messageDao)
+    val localConversationRepository = LocalConversationRepository(conversationDao)
+    val navController = rememberNavController()
+
+    VialmentorAppTheme {
+        NavHost(
+            navController = navController,
+            startDestination = "conversationsList"
+        ) {
+
+            composable("conversationsList") {
+                ConversationsListScreen(
+                    ConversationsListViewModel(localConversationRepository),
+                    navController = navController,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            composable("conversation/{conversationId}") { backStackEntry ->
+                val conversationId =
+                    backStackEntry.arguments?.getString("conversationId")?.toInt()
+                require(conversationId is Int)
+                ConversationScreen(
+                    ConversationViewModel(
+                        localMessageRepository,
+                        localConversationRepository,
+                        conversationId
+                    ),
+                    conversationId = conversationId,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
+}
