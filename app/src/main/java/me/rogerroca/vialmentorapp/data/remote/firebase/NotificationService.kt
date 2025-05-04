8948cc6f5b706a9ca7fbcef61bfb6111d41c6540
Roger Roca Arano <git@rogerroca.me>
2025-05-03 23:30:54 -0400

    feat: add notification handling in Firebase Messaging Service
    This commit implements notification handling within the `NotificationService`, which extends `FirebaseMessagingService`.
    - Adds `onNewToken` override to log new FCM registration tokens.
    - Adds `onMessageReceived` override to handle incoming remote messages.
    - Adds `showNotification` to display the received message as a notification.
    - Sets notification's title and body to received message's title and body, respectively.
    - Sets the notification's auto-cancel property to true.
    - Sets the notification's small icon.

--- app/src/main/java/me/rogerroca/vialmentorapp/data/remote/firebase/NotificationService.kt
+++ app/src/main/java/me/rogerroca/vialmentorapp/data/remote/firebase/NotificationService.kt
@@ -1,7 +1,40 @@
 package me.rogerroca.vialmentorapp.data.remote.firebase
 
+import android.app.NotificationManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import me.rogerroca.vialmentorapp.R
 
 class NotificationService : FirebaseMessagingService() {
+    private val TAG = "NotificationService"
+    /**
+     * Called if the FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
 
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        // sendRegistrationToServer(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        showNotification(message.notification!!.title!!, message.notification!!.body!!)
+    }
+
+    private fun showNotification(title: String, body: String) {
+        val builder = NotificationCompat.Builder(this, "FCM_CHANNEL")
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+
+        val manager : NotificationManager = getSystemService(NotificationManager::class.java)
+        manager.notify(0, builder.build())
+    }
 }
