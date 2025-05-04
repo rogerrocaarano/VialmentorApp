dc5ddcb8908d20258586497c482fdebc43d68f22
Roger Roca Arano <git@rogerroca.me>
2025-05-04 15:08:30 -0400

    feat: add Koin dependency for dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,11 @@ dependencies {
 
     implementation(libs.androidx.activity.ktx)
 
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
