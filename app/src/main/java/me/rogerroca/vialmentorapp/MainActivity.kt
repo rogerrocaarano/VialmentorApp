c9a4c963aa53ed8ee214603bf1aa6cf9dac542f3
Roger Roca Arano <git@rogerroca.me>
2025-05-03 22:45:24 -0400

    feat: add PermissionManager for handling runtime permissions
    This commit introduces a `PermissionManager` class to handle runtime permissions for the application, specifically for posting notifications.
    - Creates a new `PermissionManager` class that extends `ComponentActivity` to manage the permission request flow.
    - Registers an `ActivityResultLauncher` to handle the permission request result.
    - Implements `requestPermission()` function to check for `POST_NOTIFICATIONS` permission and launch the permission request dialog if necessary.
    - Implements the `showNotification()` function.
    - Modifies `MainActivity` to instantiate and use the `PermissionManager` to request the `POST_NOTIFICATIONS` permission during the `onCreate` lifecycle.
    - Updates `MyScreenContent` function, move `database`, `messageDao`, `conversationDao`, `localMessageRepository`, `localConversationRepository` and `navController` from `onCreate` function.
    - Adds `showDialog` variable for handle dialog state.

--- app/src/main/java/me/rogerroca/vialmentorapp/MainActivity.kt
+++ app/src/main/java/me/rogerroca/vialmentorapp/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -19,44 +24,58 @@ import me.rogerroca.vialmentorapp.viewmodel.ConversationViewModel
 import me.rogerroca.vialmentorapp.viewmodel.ConversationsListViewModel
 
 class MainActivity : ComponentActivity() {
+    private lateinit var permissionManager: PermissionManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        permissionManager = PermissionManager(this)
+        permissionManager.requestPermission()
+
         setContent {
-            val database = AppDb.getDatabase(this)
-            val messageDao = database.messageDao()
-            val conversationDao = database.conversationDao()
-            val localMessageRepository = LocalMessageRepository(messageDao)
-            val localConversationRepository = LocalConversationRepository(conversationDao)
-            val navController = rememberNavController()
+            var showDialog by remember { mutableStateOf(false) }
+            // This is a Composable function. The contents of setContent are composable functions
+            MyScreenContent(this)
+        }
+    }
 
-            VialmentorAppTheme {
-                NavHost(
-                    navController = navController,
-                    startDestination = "conversationsList"
-                ) {
+    @Composable
+    fun MyScreenContent(activity: MainActivity) {
+        val database = AppDb.getDatabase(activity)
+        val messageDao = database.messageDao()
+        val conversationDao = database.conversationDao()
+        val localMessageRepository = LocalMessageRepository(messageDao)
+        val localConversationRepository = LocalConversationRepository(conversationDao)
+        val navController = rememberNavController()
 
-                    composable("conversationsList") {
-                        ConversationsListScreen(
-                            ConversationsListViewModel(localConversationRepository),
-                            navController = navController,
-                            modifier = Modifier.fillMaxSize()
-                        )
-                    }
+        VialmentorAppTheme {
+            NavHost(
+                navController = navController,
+                startDestination = "conversationsList"
+            ) {
 
-                    composable("conversation/{conversationId}") { backStackEntry ->
-                        val conversationId = backStackEntry.arguments?.getString("conversationId")?.toInt()
-                        require(conversationId is Int)
-                        ConversationScreen(
-                            ConversationViewModel(
-                                localMessageRepository,
-                                localConversationRepository,
-                                conversationId
-                            ),
-                            conversationId = conversationId,
-                            modifier = Modifier.fillMaxSize()
-                        )
-                    }
+                composable("conversationsList") {
+                    ConversationsListScreen(
+                        ConversationsListViewModel(localConversationRepository),
+                        navController = navController,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+
+                composable("conversation/{conversationId}") { backStackEntry ->
+                    val conversationId =
+                        backStackEntry.arguments?.getString("conversationId")?.toInt()
+                    require(conversationId is Int)
+                    ConversationScreen(
+                        ConversationViewModel(
+                            localMessageRepository,
+                            localConversationRepository,
+                            conversationId
+                        ),
+                        conversationId = conversationId,
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
