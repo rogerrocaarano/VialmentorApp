916ab8afb5f31ef9d2a2ccbdf2842d9e465f5a45
Roger Roca Arano <git@rogerroca.me>
2025-05-03 22:19:54 -0400

    feat: add `POST_NOTIFICATIONS` permission and update dependencies
    This commit adds the `POST_NOTIFICATIONS` permission to the `AndroidManifest.xml` and updates dependencies in `libs.versions.toml` and `build.gradle.kts`.
    - Adds the `android.permission.POST_NOTIFICATIONS` permission to `AndroidManifest.xml`.
    - Adds `activity-ktx` dependency version 1.10.1 to `libs.versions.toml`.
    - Adds `androidx-activity-ktx` library in `libs.versions.toml`.
    - Adds `implementation(libs.androidx.activity.ktx)` in `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
 
+    implementation(libs.androidx.activity.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
