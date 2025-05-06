a28acc7aa3a63c339aa39f4bd77d81c2f232ccf2
Roger Roca Arano <git@rogerroca.me>
2025-05-06 12:29:16 -0400

    feat: add BottomNavBar and new conversation icon to ConversationsListScreen

--- app/src/main/java/me/rogerroca/vialmentorapp/ui/screen/ConversationsListScreen.kt
+++ app/src/main/java/me/rogerroca/vialmentorapp/ui/screen/ConversationsListScreen.kt
@@ -3,13 +3,18 @@ package me.rogerroca.vialmentorapp.ui.screen
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import me.rogerroca.vialmentorapp.R
+import me.rogerroca.vialmentorapp.ui.component.BottomNavBar
 import me.rogerroca.vialmentorapp.ui.component.ConversationListItem
 import me.rogerroca.vialmentorapp.ui.navigation.Routes
 import me.rogerroca.vialmentorapp.viewmodel.ConversationsListViewModel
@@ -23,6 +28,9 @@ fun ConversationsListScreen(
     val conversations by viewModel.conversations.collectAsState()
     val createdConversationId by viewModel.createdConversation.collectAsState()
 
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     LaunchedEffect(createdConversationId) {
         createdConversationId?.let { id ->
             navController.navigate(Routes.goToConversation(id))
@@ -31,13 +39,25 @@ fun ConversationsListScreen(
     }
 
     Scaffold(
+        bottomBar = {
+            BottomNavBar(currentRoute ?: "") { selectedItem ->
+                navController.navigate(selectedItem.route) {
+                    popUpTo(navController.graph.startDestinationId) { saveState = true }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
                     viewModel.newConversation("Test nueva conversación")
                 }
             ) {
-                // TODO: Add icon here
+                Icon(
+                    painter = painterResource(id = R.drawable.mode_comment),
+                    contentDescription = "New conversation"
+                )
             }
         },
         modifier = modifier
